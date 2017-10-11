c0587d4162ad23da5f456f98751fa175eab4ef9d
Drew Botka <drewbotka@Drews-MacBook-Pro.local>
2017-10-11 13:38:33 -0400

    initial commit

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Hero } from '../hero';
+
+import { HeroService } from '../hero.service';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+
+export class HeroesComponent implements OnInit {
+
+  constructor(private heroService: HeroService) { }
+  selectedHero: Hero;
+  // hero: Hero = {
+  //   id: 1,
+  //   name: 'Windstorm'
+  // };
+  heroes: Hero[];
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+  getHeroes(): void {
+    this.heroService.getHeroes().then(heroes => this.heroes = heroes);
+  }
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
+}
