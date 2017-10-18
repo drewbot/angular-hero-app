d9f0cb0529298d5cc36e1a6819c483ded2e69c72
Drew Botka <drewbotka@gmail.com>
2017-10-18 10:49:15 -0400

    added angular material and related dependencies, started implementing stying and components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { HttpModule } from '@angular/http';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatButtonModule, MatCheckboxModule, MatMenuModule} from '@angular/material';
 
 // Imports for loading & configuring the in-memory web api
-import { InMemoryWebApiModule } from 'angular-in-memory-web-api/in-memory-web-api.module';
-import { InMemHeroService } from './in-memory-data.service';
+// import { InMemoryWebApiModule } from 'angular-in-memory-web-api/in-memory-web-api.module';
+// import { InMemHeroService } from './in-memory-data.service';
 
 import { AppComponent } from './app.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
@@ -22,7 +24,9 @@ import { AppRoutingModule } from './app-routing.module';
     FormsModule,
     AppRoutingModule,
     HttpModule,
-    InMemoryWebApiModule.forRoot(InMemHeroService)
+    // InMemoryWebApiModule.forRoot(InMemHeroService),
+    BrowserAnimationsModule,
+    MatMenuModule
   ],
   declarations: [
     AppComponent,
